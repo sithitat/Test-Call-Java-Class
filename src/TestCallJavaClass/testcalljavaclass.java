310fae062477446b672ad96367496541a9bd3f9d
Fifth Element <sithitat@gmail.com>
2023-06-15 14:08:26 +0700

    Call new function
    Call new function WriteErrorToFileByDateTimeV2

--- src/TestCallJavaClass/testcalljavaclass.java
+++ src/TestCallJavaClass/testcalljavaclass.java
@@ -5,12 +5,27 @@
  */
 package TestCallJavaClass;
 import TestWriteLogFileClass.testwritelogfileclass;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author bcc14
  */
 public class testcalljavaclass extends javax.swing.JFrame {
-
+    public static final String formatDate = "yyyyMMdd";
+    public static final String formatDateTimeFull = "dd/MM/yyyy HH:mm:ss";
+    
     /**
      * Creates new form testcalljavaclass
      */
@@ -64,13 +79,79 @@ public class testcalljavaclass extends javax.swing.JFrame {
         // Create an instance of myLog
         testwritelogfileclass myLog = new testwritelogfileclass();
         
-        myLog.WriteErrorToFileByDateTime("ErrorTitle", "ErrorDescription");
-        myLog.WriteErrorToFileByDateTimeHourMin("ErrorTitle", "ErrorDescription");
-        myLog.WriteErrorToFile("ErrorTitle", "ErrorDescription");
-        myLog.WriteErrorToFile("ErrorTitle", new Exception("Some error message"));
+        myLog.WriteErrorToFileByDateTimeV2("ErrorTitleV2", "ErrorDescriptionV2");
+        // myLog.WriteErrorToFileByDateTime("ErrorTitle", "ErrorDescription");
+        // myLog.WriteErrorToFileByDateTimeHourMin("ErrorTitle", "ErrorDescription");
+        // myLog.WriteErrorToFile("ErrorTitle", "ErrorDescription");
+        // myLog.WriteErrorToFile("ErrorTitle", new Exception("Some error message"));
         
     }//GEN-LAST:event_btnCallClassAActionPerformed
 
+    /*
+    private void WriteErrorToFileByDateTimeV2(String sPrmErrTitle, String sPrmErrDesc) {
+        String sFileDate = getSystemDate(formatDate);
+        String sFileName = sFileDate + ".log";
+        String sPathFile = "";
+        String sErrorDisplay = "";
+
+        try {
+            Path basePath = Paths.get(".").toAbsolutePath().normalize();
+            sPathFile = basePath.resolve("Error").resolve(sPrmErrTitle).resolve(sFileName).toString();
+
+            Path parentDir = Paths.get(sPathFile).getParent();
+            if (!Files.exists(parentDir)) {
+                Files.createDirectories(parentDir);
+                Files.createFile(parentDir.resolve(sFileName));
+            }
+
+            try (PrintWriter pWriter = new PrintWriter(new BufferedWriter(new FileWriter(sPathFile, true)))) {
+                pWriter.println(getSystemDate(formatDateTimeFull) + ": ");
+                pWriter.println(sPrmErrDesc);
+            }
+        } catch (IOException ex) {
+            sErrorDisplay = DisplayLogMessage(ex);
+            Logger.getLogger(testcalljavaclass.class.getName()).log(Level.SEVERE, null, sErrorDisplay);
+        }
+    }
+    
+    private String getSystemDate(String format) {
+        try{
+            DateFormat dateFormat = new SimpleDateFormat(format,Locale.US);
+            Date date = new Date();
+            return dateFormat.format(date);
+        }
+        catch (Exception e){
+            return null;
+        }
+    }
+
+    private String DisplayLogMessage(Exception PrmEx){
+        String sReturnMessage;
+        String sErrDateTime;
+        String sErrString;
+        String sErrFull;
+        String sErrLine;
+        
+        StackTraceElement[] stackTrace = PrmEx.getStackTrace();
+        sErrDateTime = getSystemDate(formatDateTimeFull);
+
+        sErrString = PrmEx.getMessage();
+        // sErrFull = PrmEx.getStackTrace()[0].getMethodName();
+        sErrFull = stackTrace[0].getMethodName();
+        // sErrLine = String.valueOf(PrmEx.getStackTrace()[1].getLineNumber());
+        sErrLine = String.valueOf(stackTrace[1].getLineNumber());
+        
+        sErrString = sErrString + "[" + sErrFull + " - at Line No." + sErrLine + "]";
+        // sErrFull = sErrDateTime + ":" + PrmEx.getStackTrace()[1].getFileName() + " --> " + PrmEx.getStackTrace()[1].getClassName() + " --> " + PrmEx.getStackTrace()[1].getMethodName();
+        sErrFull = sErrDateTime + ":" + stackTrace[1].getFileName() + " --> " + stackTrace[1].getClassName() + " --> " + stackTrace[1].getMethodName();
+        sErrString = sErrFull + " --> " + sErrString;
+        
+        sReturnMessage = sErrString;
+        
+        return sReturnMessage;
+    }
+    */
+    
     /**
      * @param args the command line arguments
      */
